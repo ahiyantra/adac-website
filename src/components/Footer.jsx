79d0cfb0a2a98a671bf88c1cb70e18fdf929e880
ahiyantra <45235557+ahiyantra@users.noreply.github.com>
2025-01-17 21:16:04 +0200

    made corrections

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 const Footer = () => (
   <footer className="footer">
-    <p>&copy; {new Date().getFullYear()} Agastya Dental & Aesthetic Clinic (ADAC). All Rights Reserved.</p>
+    <p><small>&copy; {new Date().getFullYear()}, Agastya Dental & Aesthetic Clinic (ADAC). All Rights Reserved.</small></p>
+    <p><small><small>Made By AhiYantra (<a href="https://github.com/ahiyantra">@ahiyantra</a>)</small></small></p>
   </footer>
 );
 
